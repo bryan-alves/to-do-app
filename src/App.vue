e1cf50b5362e16a1e97386965ee034dad1ddee06
Bryan Alves <info.bryanalves@gmail.com>
2022-11-02 13:49:00 -0300

    feature: initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <img
+      alt="MyTasks logo"
+      class="logo"
+      src="@/assets/mytasks.png"
+      width="75"
+      height="75"
+    />
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/MyTasks.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1>MyTasks</h1>
+  </main>
+</template>
